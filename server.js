8ba9c44df14d219576084d42ab8c3376c0a585b7
Will Germany <germany.william@gmail.com>
2018-02-16 02:07:58 -0500

    Finished ugly code

--- server.js
+++ server.js
@@ -2,14 +2,48 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const path = require('path')
+const generate = require('nanoid/generate')
+const dotenv = require('dotenv').config()
+
+const key = () => generate('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 6)
+
+const toMegaBytes = (x) => x * 1000 * 1000
+
+let imageURL = undefined
+
+const storageOptions = {
+  destination: process.env.UPLOAD_DESTINATION,
+  filename: function (req, file, callback) {
+    imageURL = key() + path.extname(file.originalname)
+    callback(null, imageURL)
+  }
+}
+
+const storage = multer.diskStorage(storageOptions)
+
+const limits = {
+  fileSize: toMegaBytes(5)
+}
+
+const upload = multer({ storage, limits }).any()
 
 const app = express()
 const port = process.env.PORT || 8000
 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.post('/upload', async (req, res) => res.send({ message: 'Upload feature coming soon!' }))
+app.post('/upload', (req, res) => {
+  upload(req, res, (error) => {
+    if (error) {
+      res.status(500).send({ error })
+    } else if (req.body.secretKey !== process.env.SECRET_KEY) {
+      res.status(500).send({ error: 'Unauthorized.' })
+    } else {
+      res.status(200).send({ url: path.join(__dirname, '/uploads/' + imageURL)})
+    }
+  })
+})
 
-app.listen(port, async () => console.log(`Server started on port ${port}.`))
+app.listen(port, () => console.log(`Server started on port ${port}.`))
 
 module.exports = app
